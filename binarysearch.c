5ae239b09a9083e15afae72837e7202e80ae6fa9
ANKUSH Banerjeee <banerjeeankush184@gmail.com>
2022-08-13 13:29:09 +0530

    stack using queue done

---
+++ binarysearch.c
@@ -0,0 +1,94 @@
+#include <stdio.h>
+void display(int arr[], int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+}
+void sorted(int arr[], int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        int min = i;
+        for (int j = i + 1; j < n; j++)
+        {
+            if (arr[j] < arr[min])
+            {
+                min = j;
+            }
+        }
+        if (min != i)
+        {
+            int temp = arr[min];
+            arr[min] = arr[i];
+            arr[i] = temp;
+        }
+    }
+}
+
+void binarysearch(int arr[], int n, int element)
+{
+    int low, mid, high;
+    low = 0;
+    high = n - 1;
+    while (low <= high)
+    {
+        mid = (low + high) / 2;
+        if (arr[mid] == element)
+        {
+            printf("\n The element is found at index %d", mid);
+            break;
+        }
+        else if (arr[mid] < element)
+        {
+            low = mid + 1;
+        }
+        else
+        {
+            high = mid - 1;
+        }
+    }
+}
+int main()
+{
+    // int n;
+    // printf("Enter number of elements: ");
+    // scanf("%d", &n);
+    // int arr[n];
+    // for (int i = 0; i < n; i++)
+    // {
+    //     printf("\n Enter an element: ");
+    //     scanf("%d", &arr[i]);
+    // }
+    // printf("\n Here is your array: ");
+    // display(arr, n);
+    // int element;
+    // /*int element;
+    // printf("\n Enter the element to be searched: ");
+    // scanf("%d", &element);
+    // linearsearch(arr,n,element);*/
+    // sorted(arr, n);
+    // printf("\n Sorted array: ");
+    // display(arr, n);
+    // printf("\n Enter element to be found: ");
+    // scanf("%d", element);
+
+    int n;
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        printf("\n Enter an element: ");
+        scanf("%d", &arr[i]);
+    }
+    sorted(arr, n);
+    printf("\n Sorted array: ");
+    display(arr, n);
+    printf("\n Enter element to be found: ");
+    int element;
+    scanf("%d", &element);
+    binarysearch(arr, n, element);
+    return 0;
+}
--- queueusingstack.cpp
+++ queueusingstack.cpp
@@ -1,109 +1,52 @@
-// #include "bits/stdc++.h"
-// #include <stack>
-// using namespace std;
-// class que
-// {
-//     stack<int> s1;
-//     stack<int> s2;
-
-// public:
-//     void push(int x)
-//     {
-//         s1.push(x);
-//     }
-//     int pop()
-//     {
-//         if (s1.empty() && s2.empty())
-//         {
-//             cout << "queue is empty";
-//             return -1;
-//         }
-//         if (s2.empty())
-//         {
-//             while (!s1.empty())
-//             {
-//                 s2.push(s1.top());
-//                 s2.pop();
-//             }
-//         }
-//         int topval = s2.top();
-//         s2.pop();
-//         return topval;
-//     }
-//     bool empty()
-//     {
-//         if (s1.empty() && s2.empty())
-//         {
-//             return true;
-//         }
-//         return false;
-//     }
-// };
-// int main()
-// {
-//     que q;
-//     q.push(1);
-//     q.push(2);
-//     cout << q.pop() << "\n";
-//     return 0;
-// }
-// CPP program to implement Queue using
-// two stacks with costly enQueue()
-#include <bits/stdc++.h>
+#include "bits/stdc++.h"
+#include <iostream>
 using namespace std;
-
-struct Queue
+class Que
 {
-    stack<int> s1, s2;
+    stack<int> s1;
+    stack<int> s2;
 
-    void enQueue(int x)
+public:
+    void push(int x)
     {
-        // Move all elements from s1 to s2
-        while (!s1.empty())
-        {
-            s2.push(s1.top());
-            s1.pop();
-        }
-
-        // Push item into s1
         s1.push(x);
-
-        // Push everything back to s1
-        while (!s2.empty())
-        {
-            s1.push(s2.top());
-            s2.pop();
-        }
     }
-
-    // Dequeue an item from the queue
-    int deQueue()
+    int pop()
     {
-        // if first stack is empty
-        if (s1.empty())
+        if (s1.empty() && s2.empty())
         {
-            cout << "Q is Empty";
-            exit(0);
+            cout << "queue is empty";
+            return -1;
         }
-
-        // Return top of s1
-        int x = s1.top();
-        s1.pop();
-        return x;
+        if (s2.empty())
+        {
+            while (!s1.empty())
+            {
+                s2.push(s1.top());
+                s1.pop();
+            }
+        }
+        int topval = s2.top();
+        s2.pop();
+        return topval;
     }
 };
-
-// Driver code
 int main()
 {
-    Queue q;
-    q.enQueue(1);
-    q.enQueue(2);
-    q.enQueue(3);
-
-    cout << q.deQueue() << '\n';
-    cout << q.deQueue() << '\n';
-    cout << q.deQueue() << '\n';
+    Que q;
+    q.push(1);
+    q.push(2);
+    q.push(3);
+    q.push(4);
+    q.push(4);
+    cout << q.pop();
+    q.push(5);
+    cout << q.pop() << "\n";
+    cout << q.pop() << "\n";
+    cout << q.pop() << "\n";
+    cout << q.pop() << "\n";
+    cout << q.pop() << "\n";
+    cout << q.pop() << "\n";
 
     return 0;
 }
